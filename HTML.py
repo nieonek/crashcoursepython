1e93b47fb9c042ea35358bd8a8b33888da916be4
nieonek <nieonek@gmail.com>
2021-11-26 18:12:36 +0100

    regexy

---
+++ HTML.py
@@ -0,0 +1,72 @@
+STRUKTURA
+
+<html>
+<head>
+    meta data
+</head>
+<body>
+    page content
+</body>
+</html>
+
+
+LINK
+<p>
+Kliknij zeby przejsc do strony 2
+<a href="http://www.jakastsronka.pl/page2.htm"> NAST STRONA/PAGE 2 </a>
+</p?
+
+LINK W NOWYM OKNIE
+
+<a href="http://www.linkwnowymoknie.com" target="_blank">w nowej zakladce</a>.
+
+HEADER
+
+<h1>NAGLOWEK</h1>
+
+
+KOMENTARZE
+
+<!--    cos cos cos -->
+
+
+PARAGRAPH
+
+<p>    jakis tekst zdanie itepe </p>
+
+BOLD
+
+<strong>WYBOLDOWANE</strong>
+
+KURSYWA
+
+<em>kursywapisane</em>
+
+ZDJECIE
+
+<img src="zdjecie.png">
+
+LISTA
+<ul>
+<li><p>tekst</p></li> * kropka do wypunktowania + element listy
+<li><p>tekst</p></li>
+<li><p>tekst</p></li>
+</ul>
+
+TABLES
+
+<table>
+    <tr>
+        <th>MAKE</th>
+        <th>MODEL</th>
+        <th>MILEAGE</th>
+    </tr>
+    <tr>
+        <td>FORD</td><td>MAZDA</td>
+        <td>1432
+        <td>aaa
+    </tr>
+</table>
+
+MAKE MODEL MILEAGE
+FORD MAZDA 1432    aaaa
